693bf5427ebe8b3b4eb3330b5c2aff5387bc860f
alex <desorbaix@free.fr>
2020-03-11 19:40:40 +0100

    template files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mon magasin php</title>
+        <link rel="stylesheet" href="styles/styles.css">
+    </head>
+    <body>
+        <div id="main">
+            <h1>Bienvenue sur le template de site php</h1>
+        </div>
+    </body>
+</html>
