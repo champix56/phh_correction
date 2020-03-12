16b225ca2e96c033e985cd3ff1f7734c6eed314b
champix56 <desorbaix.formation@free.fr>
2020-03-12 14:27:34 +0100

    insetion de la session pour authent

--- core/core.php
+++ core/core.php
@@ -3,8 +3,27 @@
         global $_DEBUG;
         if(isset($_DEBUG) && $_DEBUG==true)var_dump($variable);
     }
+    function disconnect(){
+        session_unset();
+    }
+    function checkAuthent(){
+        if(isset($_POST["login"]))
+        {
+            global $db;//usage de la variable global db innexistante dans ma fonction
+            $valid=$db->authent($_POST["login"],$_POST["password"]);
+            //echo $valid? 'user OK': 'pb user';
+            if($valid)
+            {
+                $_SESSION['uid']=$_POST["login"];
+                header("Location:index.php");
+            }
+        }
+    }
     include_once('mysql.php');
     //je creer l'objet sql pour ma page 
-    $db=new BaseDeDonnees();
-    d_vardump($db);
+    $db=new BaseDeDonnees();  // var_dump($db);
+    checkAuthent();
+
+
+ 
 ?>
